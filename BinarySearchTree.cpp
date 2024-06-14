81b4b5a77b1c25d33cb7fee62de6122b5695d339
halimbagusalhakim <halsat132@gmail.com>
2024-06-14 14:08:57 +0700

    pengkondisian jika value elemtn kurang dari ccurentNode maka current node menunjuk leftchild

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -56,8 +56,9 @@ public:
         parent = NULL;
         while ((currentNode != NULL)&& (currentNode->info != element)) {
             parent = currentNode;
-           
-             
+            if (element < currentNode->info)
+                currentNode = currentNode->leftchild;
+                 
         }
     }
 };
