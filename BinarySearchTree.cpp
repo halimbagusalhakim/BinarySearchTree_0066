ae293e772db929523a4c44346ff452532f28701f
halimbagusalhakim <halsat132@gmail.com>
2024-06-14 13:47:05 +0700

    membuat classs node uang berisi member info, leftchild, right child, dan semua constructor

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -1,3 +1,17 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class Node {
+    string info;
+    Node* leftchild;
+    Node* rightchild;
+
+    //cconstructor for the node class
+    Node(string i, Node* l, Node* r) {
+        info = i;
+        leftchild = l;
+        rightchild = r;
+    }
+
+};
