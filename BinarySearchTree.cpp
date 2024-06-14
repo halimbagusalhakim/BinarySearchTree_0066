6a7cb36ad8038bc08db8033a573dcfd1c8b4dcd1
halimbagusalhakim <halsat132@gmail.com>
2024-06-14 13:54:23 +0700

    membuat isi method insert

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -26,6 +26,13 @@ Public:
     void insert(string element) //insert a node in teh binary search tree
     {
         Node* newNode = new Node(element, NULL, NULL);
+        newNode->info = element; // assign value to the data field of the new node
+        newNode->leftchild = NULL // make the left child of the nw node point to NULL
+        newNode->rightchild = NULL // make the right child of the nw node point to null
+
+        Node* parent = NULL;
+        Node* current Node = NULL;
+        search(element, parent, currentNode);//locate the node which will be the parent of the node to be inserted
         
     }
 };
