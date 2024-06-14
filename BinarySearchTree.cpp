9c0075602a94746ccd4b187477a5fad7c842cc7d
halimbagusalhakim <halsat132@gmail.com>
2024-06-14 13:59:56 +0700

    pengkodisiain jika value element lebih besar dari parent maka right child jadi new node

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -43,5 +43,9 @@ Public:
         {
             parent->leftchild = newNode;//make the left child of the parent point to the new node
         }
+        else if(element > parent->info)//if the value in the data field of the new ndoe is greater than that of the parent
+        {
+            parent->rightchild = newNode;//make the right child of the parent point to the new node
+        }
     }
 };
